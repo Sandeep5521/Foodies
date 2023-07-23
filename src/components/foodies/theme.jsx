41e4092813d54092439bbad4bdcae2b404295445
Sandeep Sahu <sundeep5521@gmail.com>
2023-07-23 14:41:19 +0530

    modified:   src/components/foodies/theme.jsx

--- src/components/foodies/theme.jsx
+++ src/components/foodies/theme.jsx
@@ -6,7 +6,7 @@ const Mode = ({ parent }) => {
     const [theme, setTheme] = useState("light");
     let logo = (theme == 'light') ? dlogo : llogo;
     return (
-        <div className="h-10 w-10 sm:h-12 sm:w-12 bg-cover fixed top-[93%] hover:scale-105 cursor-pointer bg-black dark:bg-white flex place-content-center rounded-full left-[87%] sm:left-[91%] sm:top-[93%] xl:top-[90%] xl:left-[95%] p-1" onClick={() => {
+        <div className="h-10 w-10 sm:h-12 sm:w-12 bg-cover fixed top-[92%] hover:scale-105 cursor-pointer bg-black dark:bg-white flex place-content-center rounded-full left-[87%] sm:left-[91%] sm:top-[93%] xl:top-[90%] xl:left-[95%] p-1" onClick={() => {
             if (theme == 'light') setTheme('dark');
             else setTheme('light');
             parent();
